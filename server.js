d46b3181106f535f11e7738962a25322092d6595
Md Jishan Ansari <86798430+md-jishan-ansari@users.noreply.github.com>
2021-10-24 21:53:01 +0530

    v1.0.4

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use('/posts', postRouter);
 app.use('/user', userRouter);
 
 if (process.env.NODE_ENV === 'production') {
-    app.uses(express.static(path.join(__dirname, '/client/build')));
+    app.use(express.static(path.join(__dirname, '/client/build')));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
