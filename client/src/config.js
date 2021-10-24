e3854ba8ed3624f725f3df4c174246e8ea96f1e1
Md Jishan Ansari <86798430+md-jishan-ansari@users.noreply.github.com>
2021-10-24 22:35:07 +0530

    config.js changed

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 let DB_ROUTE;
 if (process.env.NODE_ENV === 'production') {
-    DB_ROUTE = "https://memories202021.herokuapp.com/";
+    DB_ROUTE = "https://memories2022.herokuapp.com/";
 } else {
     DB_ROUTE = "http://localhost:8000";
 }
